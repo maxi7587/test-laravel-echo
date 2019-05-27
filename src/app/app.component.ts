484caa77f5421a4ff6324dae53cbee561472b9df
maxi7587 <maxi7587@hotmail.com>
2019-05-27 11:00:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,32 @@
 import { Component } from '@angular/core';
+import Echo from 'laravel-echo';
+
+// At the top of the file
+declare global {
+    interface Window { io: any; }
+    interface Window { Echo: any; }
+    interface Window { Pusher: any; }
+}
+
+// declare var Echo: any;
+
+window.io = window.io || require('socket.io-client');
+window.Echo = window.Echo || {};
+
+
+window.Pusher = require('pusher-js');
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'CoolKey',
+  wsHost: 'apicultor-22.nanoapp.io',
+  wsPort: 6001,
+  disableStats: true,
+});
+
+window.Echo.channel('test-channel')
+.listen('.App\\SocketTesterEvent', (data) => {
+    console.log('From laravel echo: ', data);
+});
 
 @Component({
   selector: 'app-root',
@@ -6,5 +34,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-laravel-echo';
+    title = 'angular-laravel-echo';
+
+    public constructor() {
+        console.log('will call window.Echo');
+
+        // window.Echo = new Echo({
+        //     broadcaster: 'socket.io',
+        //     host: 'https://host-address.com:6001'
+        // });
+    }
+
 }
